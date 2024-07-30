d3fb418f88997040d44f75d6d1afc715e1ad6182
NikitaChebotov <nikitachebotov@gmail.com>
2024-07-30 14:41:42 +0900

    text-area placeholder fix

--- src/app/components/ContactForm/ContactForm.tsx
+++ src/app/components/ContactForm/ContactForm.tsx
@@ -171,14 +171,14 @@ export default function ContactForm() {
 							id='message'
 							{...register('message')}
 							placeholder=' '
-							className='block w-full px-2.5 pb-2.5 pt-4 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-0 focus:border-primary-500 peer'
+							className='block w-full px-2.5 pb-2.5 pt-8 sm:pt-4 bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-0 focus:border-primary-500 peer'
 							rows={4}
 						/>
 						<label
 							htmlFor='message'
 							className='absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75  top-4 z-10 origin-[0] bg-gray-50 px-2 peer-focus:px-2 peer-focus:text-primary-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 left-1'
 						>
-							Краткое описание проекта (задачи, цели)
+							Краткое описание проекта (цели, задачи)
 						</label>
 						{errors.message && (
 							<span className='text-red-500'>{errors.message.message}</span>
